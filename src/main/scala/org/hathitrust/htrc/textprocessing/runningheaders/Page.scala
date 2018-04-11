a43ffe5aa265c5d116ef21e720597fd491338e16
Boris Capitanu <borice@hotmail.com>
2018-04-11 11:37:06 -0500

    Small refactor

--- src/main/scala/org/hathitrust/htrc/textprocessing/runningheaders/Page.scala
+++ src/main/scala/org/hathitrust/htrc/textprocessing/runningheaders/Page.scala
@@ -4,20 +4,19 @@ package org.hathitrust.htrc.textprocessing.runningheaders
   * Trait defining the notion of a page, as a collection of lines of text
   */
 trait Page {
+  type Lines = IndexedSeq[String]
 
   /**
     * Returns the lines of text on the page
     *
     * @return The lines of text on the page
     */
-  def textLines: IndexedSeq[String]
+  def textLines: Lines
 
   /**
     * Returns the text on the page
     *
-    * @param sep (Optional) The separator to use when joining the lines on the page
     * @return The text on the page
     */
-  def text(sep: String = "\n"): String = textLines.mkString(sep)
-
+  def text: String = textLines.mkString("\n")
 }
