150a07569920bbe5d10d19e1207421cd15f3bba9
Boris Capitanu <borice@hotmail.com>
2018-07-05 00:59:43 -0500

    Added Java-friendly(er) wrapper

---
+++ src/main/scala/org/hathitrust/htrc/textprocessing/runningheaders/java/PageStructureParser.scala
@@ -0,0 +1,42 @@
+package org.hathitrust.htrc.textprocessing.runningheaders.java
+
+import org.hathitrust.htrc.textprocessing.runningheaders.utils.TriFunction
+import org.hathitrust.htrc.textprocessing.runningheaders.{Page, PageStructure, PageStructureParser => ScalaPageStructureParser}
+
+import scala.collection.JavaConverters._
+
+object PageStructureParser {
+
+  object StructureParserConfig {
+    def defaultConfig: StructureParserConfig =
+      new StructureParserConfig(
+        windowSize = 6,
+        minSimilarityScore = 0.7d,
+        minClusterSize = 3,
+        maxNumHeaderLines = 3,
+        maxNumFooterLines = 3
+      )
+  }
+
+  sealed class StructureParserConfig(val windowSize: Int,
+                                     val minSimilarityScore: Double,
+                                     val minClusterSize: Int,
+                                     val maxNumHeaderLines: Int,
+                                     val maxNumFooterLines: Int)
+
+  trait StructuredPage extends Page with PageStructure
+
+  def parsePageStructure[T <: Page, U <: StructuredPage](pages: java.util.List[T],
+                                                         parserConfig: StructureParserConfig,
+                                                         fun: TriFunction[T, Integer, Integer, U]): java.util.List[U] =
+    ScalaPageStructureParser.parsePageStructure(
+      pages = pages.asScala.toList,
+      windowSize = parserConfig.windowSize,
+      minSimilarityScore = parserConfig.minSimilarityScore,
+      minClusterSize = parserConfig.minClusterSize,
+      maxNumHeaderLines = parserConfig.maxNumHeaderLines,
+      maxNumFooterLines = parserConfig.maxNumFooterLines,
+      builder = (page: T, headerLinesCount, footerLinesCount) => fun.apply(page, headerLinesCount, footerLinesCount)
+    ).asJava
+
+}
